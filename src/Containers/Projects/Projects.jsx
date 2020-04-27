073bb9fbd7ca12f64f638c5c18cf9f6076efb34e
Elliott <enix002@aucklanduni.ac.nz>
2020-04-27 17:56:04 +1200

    Projects content block done - TODO mobile scaling

--- src/Containers/Projects/Projects.jsx
+++ src/Containers/Projects/Projects.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-//CSS
+//CSS and Assets
 import classes from './Projects.module.css';
+import MindMapeImg from '../../assets/images/MindMape/loginpage.png';
+import BurgerBuilderImg from '../../assets/images/BurgerBuilder/BuildPage.png';
+import ThisWebsiteImg from '../../assets/images/ThisWebsite/thisSiteFaviconBig.png';
+
 //Components
 import Aux from '../../hoc/Auxillary/Auxillary';
 
@@ -9,6 +13,28 @@ const Projects = () => {
     return (
         <Aux>
             <div className={classes.Projects}>
+                <div className={classes.MindMape}>
+                    <div className={classes.Blurb}>
+                        <h1>MindMape</h1>
+                        <p>This is project I completed in my PostGraduate Certificate in Information Technology.</p>
+                    </div>
+                    <img src={MindMapeImg} alt="MindMapeImg" />
+                </div>
+                <div className={classes.BurgerBuilder}>
+                    <img src={BurgerBuilderImg} alt="BurgerBuilderImg" />
+                    <div className={classes.Blurb}>
+                        <h1>BurgerBuilder</h1>
+                        <p>Hungry? I created a BurgerBuilder webapp in the Udemy React course by AcadeMind.</p>
+                    </div>
+
+                </div>
+                <div className={classes.ThisWebsite}>
+                    <div className={classes.Blurb}>
+                        <h1>This Website!</h1>
+                        <p>Hungry? I created a BurgerBuilder webapp in the Udemy React course by AcadeMind.</p>
+                    </div>
+                    <img src={ThisWebsiteImg} alt="ThisWebsiteImg" />
+                </div>
             </div>
         </Aux>
     )
