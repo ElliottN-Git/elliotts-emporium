489171af1f33ec4d59691a77d6b6600fd7efa676
Elliott <enix002@aucklanduni.ac.nz>
2020-06-16 22:59:15 +1200

    Updating header

--- src/Components/UI/Header/Header.jsx
+++ src/Components/UI/Header/Header.jsx
@@ -4,30 +4,29 @@ import React, { Component } from 'react';
 
 //CSS
 import classes from './Header.module.css';
+
+import heroImg from '../../../assets/images/heroImgBeach.png';
+import headerText from '../../../assets/images/heyImElliottTextTransparentBG.png';
 import headShot from '../../../assets/images/EN-headshotCircle.png';
 import enhancedBackground from '../../../assets/images/headerImgSpace.jpg';
 
 class Header extends Component {
-    state = {
-        bgLoaded: false,
-        headShotLoaded: false
-    }
 
     render() {
         return (
-            <div className={classes.Header} style={{backgroundImage: this.state.bgLoaded ? `url(${enhancedBackground})` : ''}}>
-                <img src={enhancedBackground} alt="enhBG" onLoad={() => this.setState({bgLoaded: true})}
-                style={{
-                    visibility: "hidden",
-                    position: "fixed",
-                    height: "0",
-                    width: "0"}} />
-                <img src={headShot} alt="Headshot" 
-                    onLoad={() => {
-                        this.setState({headShotLoaded: true});
-                    }}/>
-                <h1>Hi! I'm Elliott.</h1>
-                {this.props.children}
+            // <div className={classes.Header} style={{backgroundImage: this.state.bgLoaded ? `url(${enhancedBackground})` : ''}}>
+            //     <img src={enhancedBackground} alt="enhBG" onLoad={() => this.setState({bgLoaded: true})}
+            //     style={{
+            //         visibility: "hidden",
+            //         position: "fixed",
+            //         height: "0",
+            //         width: "0"}} />
+            //     <img src={headShot} alt="Headshot" 
+            //         onLoad={() => {
+            //             this.setState({headShotLoaded: true});
+            //         }}/>
+            <div className={classes.Header} style={{backgroundImage: `url(${heroImg})`}}>
+                <img src={headerText} />
             </div>
         )
     }
