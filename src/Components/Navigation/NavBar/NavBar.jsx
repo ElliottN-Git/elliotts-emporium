c330fd14889d030afbd5189e3d8174bee4d50282
Elliott <enix002@aucklanduni.ac.nz>
2020-07-18 16:19:58 +1200

    cleared warning

--- src/Components/Navigation/NavBar/NavBar.jsx
+++ src/Components/Navigation/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 //CSS
 import classes from './NavBar.module.css';
 import ENLogo from '../../../assets/images/ThisWebsite/ENLogo.png';
