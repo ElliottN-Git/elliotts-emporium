20ac9423449f600f6967b6b7b0179677d9f7426c
Elliott Nixon <contact@elliottnixon.com>
2023-08-28 21:40:04 +0100

    Added link to mindMAPE website

--- src/Containers/Routes/Home.jsx
+++ src/Containers/Routes/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 //CSS and assets
 // import aboutMeBackgroundImage from '../../assets/images/chemistrybg.jpg';
 
